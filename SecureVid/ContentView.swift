da0f68a982092fd1c15508f78be0cd862213a682
Sachin Khanal <sackhanaal@gmail.com>
2020-03-10 22:52:51 -0500

    Initial Commit

---
+++ SecureVid/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SecureVid
+//
+//  Created by Sachin Khanal on 3/10/20.
+//  Copyright © 2020 Sachin Khanal. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
